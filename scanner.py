0fdff8859c3435c7c888959bc56dee5bd4f511e0
pranaygoud36 <pranaygoud36@gmail.com>
2017-12-17 13:03:44 +0530

    Add new feature #disconnect node

--- scanner.py
+++ scanner.py
@@ -147,19 +147,13 @@ def disconnect():
 def sendPacket(my_mac,gateway_ip,target_ip,target_mac):#need to be modify a bit
 	ether = Ether()
 	ether.src = my_mac
+	ether.dst = target_mac
 	
 	arp=ARP()
 	arp.psrc = gateway_ip
 	arp.hwsrc = my_mac
-	
-	arp=arp
 	arp.pdst = target_ip
 	arp.hwdst = target_mac
-	
-	ether = ether
-	ether.src = my_mac
-	ether.dst = target_mac
-	
 	arp.op=2
 	
 	packet = ether / arp
